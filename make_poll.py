4accd2a92c34be5e47e3ca742c8c848f7b36e113
Jeff Zhang <jeff.2.zhang@nokia.com>
2019-04-23 21:42:37 -0400

    refactor logger and ignore log file

--- make_poll.py
+++ make_poll.py
@@ -1,12 +1,17 @@
+"""
+Templated xml generator for SecurePoll.
+Original author: Fee Christoph
+"""
+
 import json
 import os
-import shutil
+import logging
+import datetime
 import click
 import jinja2
 import uuid
 import gspread
 import smtplib
-import ssl
 from oauth2client.service_account import ServiceAccountCredentials
 from datetime import datetime
 from email.mime.text import MIMEText
@@ -20,9 +25,13 @@ CRED = os.path.join(os.path.dirname(__file__), 'makepoll-9b8df964291c.json')
 REPLACE = os.path.join(os.path.dirname(__file__), 'REPLACE')
 LASTPOLLID = os.path.join(os.path.dirname(__file__), 'LASTPOLLID')
 
-
-# response sheet name
-FORM_NAME = "Escher_Poll_{}_{}_{}".format('01', '23', '19')
+logger = logging.getLogger("make_poll.py")
+# If verbose is set, also print debug logs to the log file, else stop at info
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler('make_poll.log')
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+logger.addHandler(fh)
 
 """
 	Set up on the developer console (one time):
@@ -77,14 +86,14 @@ def next_available_row(wks):
 
 
 # authenticate access to the Google Form responses
-def getForm():
-	print(CRED)
+def getForm(form):
+	logger.debug("cred file:" + CRED)
 	credentials = ServiceAccountCredentials.from_json_keyfile_name(CRED, scope)
-	print("GETTING FORM")
+	logger.info("GETTING FORM")
 	gc = gspread.authorize(credentials)
-	print("AUTHORIZED")
+	logger.info("AUTHORIZED")
 	# Sample form response sheet name
-	wks = gc.open(FORM_NAME).sheet1
+	wks = gc.open(form).sheet1
 	# wks is much like panda, can then do data manipulation and stuff
 	return wks
 
@@ -159,15 +168,27 @@ def sendEmail():
 
 @click.command()
 @click.option('--verbose', '-v', is_flag=True, help='Print more output.')
+@click.option('--date', '-d', nargs=3, help='YEAR MON DAY, e.g. 2019 4 23')
 # @click.argument('input_dir', required=True)
-def main(verbose):
-	"""Templated xml generator for SecurePoll."""
+def main(verbose, date):
+
+	if len(date) == 3:
+		year = date[0]
+		month = date[1]
+		day = date[2]
+	else:
+		year = datetime.datetime.now().year
+		month = datetime.datetime.now().month
+		day = datetime.datetime.now().day
+
+	# response sheet name
+	form = "Escher_Poll_{month}_{day}_{year}".format(year=year, month=month, day=day)
 	try:
 		#sendEmail()
-		print("STARTING")
+		logger.info("START: Start collecting poll results")
 		# worksheet object to get data from form
-		wks = getForm()
-		print("GOT FORM")
+		wks = getForm(form)
+		logger.info("SUCCESS: retrieved form from Google")
 
 		# get last submitted entry of spreadsheet (last submitted form)
 		first_empty = next_available_row(wks)
@@ -175,11 +196,11 @@ def main(verbose):
 		last_row = wks.row_values(last_row)
 
 		# TODO - FORMAT FOR EMPTY ROWS
-		print(last_row)
+		logger.debug("last row: {}".format(last_row))
 
 		pollTimestamp, meetDate, startDate, endDate, props, nonstudents, tempstays, email = last_row[0], last_row[1], last_row[2], last_row[3], last_row[4], last_row[5], last_row[6], last_row[7]
 
-		print(pollTimestamp)
+		logger.info("poll timestamp = {}".format(pollTimestamp))
 
 		# check that start and end dates are in correct order in time
 		checkTime(startDate, endDate)
@@ -202,21 +223,21 @@ def main(verbose):
 			lastpoll = f.readline().strip()
 			last_id = f.readline().strip()
 			last_timestamp = f.readline().strip()
-		print("LAST TIME: " + last_timestamp)
-		print("NOW TIME: " + pollTimestamp)
-		print("LAST ID: " + last_id)
+		logger.debug("LAST TIME: " + last_timestamp)
+		logger.debug("NOW TIME: " + pollTimestamp)
+		logger.debug("LAST ID: " + last_id)
 		if last_timestamp == pollTimestamp:
 			with open(REPLACE, 'w') as f:
 				f.write("done")
 			return
 
-		print(lastpoll)
-		print(meetDate)
+		logger.debug("LAST POLL: " + lastpoll)
+		logger.debug("MEET DATE: " + meetDate)
 		if lastpoll != config["out_name"]:
-			print("NEW POLL")
+			logger.info("NEW POLL")
 			rand_id = uuid.uuid1().int
 			rand_id = str(rand_id)
-			print("RANDID " + rand_id)
+			logger.info("RANDID " + rand_id)
 			rand_id = rand_id[0:8]
 
 			# update replace file
@@ -237,8 +258,7 @@ def main(verbose):
 			f.write(str(context["id"]) + '\n')
 			f.write(pollTimestamp)
 
-
-		print("ID " + str(context["id"]))
+		logger.debug("ID " + str(context["id"]))
 		context["admins"] = "FeeFiFoeFum"
 		context["intro"] = "Click the buttons to vote Yes, Abstain, or No. Information on all proposals can be found in the email."
 		context["questions"] = []
@@ -260,12 +280,12 @@ def main(verbose):
 
 		config["context"] = context
 
-		print("FILLED ARRAY")
+		logger.info("FILLED ARRAY")
 
 		# Get template and render template
 		input_dir = os.path.dirname(os.path.abspath(__file__))
 		template_dir = input_dir + '/templates'
-		print(template_dir)
+		logger.debug("template dir: " + template_dir)
 
 		output_location = os.path.join(input_dir, 'out')
 
@@ -275,7 +295,6 @@ def main(verbose):
 		template_env = jinja2.Environment(
 										  loader=jinja2.FileSystemLoader(template_dir),
 										  autoescape=jinja2.select_autoescape(['html', 'xml']),
-
 		)
 
 		out_file = os.path.join(output_location, config['out_name'])
@@ -287,39 +306,38 @@ def main(verbose):
 		with open(out_file, 'w') as file:
 			file.write(template_out)
 
-		if verbose:
-			print('Rendered ' + config['template'] + ' -> ' + out_file)
+		logger.info('Rendered ' + config['template'] + ' -> ' + out_file)
 
 	except jinja2.UndefinedError:
-		print('Error_Jinja: Template tried to operate on Undefined')
+		logger.error('Error_Jinja: Template tried to operate on Undefined')
 		exit(1)
 
 	except jinja2.TemplateNotFound:
-		print('Error_Jinja: Template not found')
+		logger.error('Error_Jinja: Template not found')
 		exit(1)
 
 	except jinja2.TemplateAssertionError:
-		print('Error_Jinja: Assertion error')
+		logger.error('Error_Jinja: Assertion error')
 		exit(1)
 
 	except jinja2.TemplateSyntaxError:
-		print('Error_Jinja: Template syntax error')
+		logger.error('Error_Jinja: Template syntax error')
 		exit(1)
 
 	except jinja2.TemplateError:
-		print('Error_Jinja: Template Error')
+		logger.error('Error_Jinja: Template Error')
 		exit(1)
 
 	except json.JSONDecodeError:
-		print('Error_JSON: Decoding error')
+		logger.error('Error_JSON: Decoding error')
 		exit(1)
 
-	except FileNotFoundError:
-		print('Error_FileNotFound: could not find file')
+	except FileNotFoundError as e:
+		logger.error('FileNotFound: {}'.format(e))
 		exit(1)
 
 	except InvalidDateError as error:
-		print('Error_InvalidDate: poll closes before it begins. Please modify endDate and startDate')
+		logger.error('Error_InvalidDate: poll closes before it begins. Please modify endDate and startDate')
 		exit(1)
 
 if __name__ == "__main__":
