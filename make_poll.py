e858dde57ebde715f44ef713e7f6722b0825efb5
Jeff Zhang <jeff.2.zhang@nokia.com>
2019-04-23 22:40:00 -0400

    take setup out of main script
    as now we have readme

--- make_poll.py
+++ make_poll.py
@@ -24,22 +24,6 @@ LASTPOLLID = os.path.join(os.path.dirname(__file__), 'LASTPOLLID')
 # response sheet name
 FORM_NAME = "Escher_Poll_{}_{}_{}".format('01', '23', '19')
 
-"""
-	Set up on the developer console (one time):
-	1. Create service account
-	2. Enable Google Sheet API
-	3. Get credentials for the API with service account
-	4. Set CRED variable to the path to the credentials file
-	"""
-
-"""
-	Set up before running form response parser (every poll):
-	1. Go to response tab of the form
-	2. Click the green icon (view response in sheets)
-	3. Create a Google sheet with name "Escher_Poll_mm_dd_yy" or something like that
-	4. Share sheet with service account email (felichri@makepoll-224313.iam.gserviceaccount.com) for now
-	TODO: hard code in the sharing?
-	"""
 
 # populate format
 """
